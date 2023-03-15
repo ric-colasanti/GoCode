e7eae488ff487ed58dc29d324d205e133348bd5b
ric-colasanti <developer.colasanti@gmail.com>
2023-03-15 19:28:46 +0000

    csvfileviewer

--- csvfileviewer/main.go
+++ csvfileviewer/main.go
@@ -9,3 +9,61 @@ import (
 func main() {
 	fmt.Println(getdirectory.ReadDir("./"))
 }
+
+/*
+
+import (
+	"fmt"
+	"net/http"
+	"os/exec"
+	"runtime"
+
+	"github.com/gorilla/mux"
+	"github.com/ric-colasanti/MicoSimViewer/microsimserver"
+)
+
+func openbrowser(url string) {
+	var err error
+
+	switch runtime.GOOS {
+	case "linux":
+		fmt.Println("Linux", url)
+		err = exec.Command("xdg-open", url).Start()
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		err = exec.Command("open", url).Start()
+	default:
+		err = fmt.Errorf("unsupported platform")
+	}
+	if err != nil {
+		fmt.Println(err)
+	}
+
+}
+
+func main() {
+	router := mux.NewRouter()
+
+	// Serve static files
+	s := http.StripPrefix("/js/", http.FileServer(http.Dir("./js/")))
+	router.PathPrefix("/js/").Handler(s)
+
+	router.HandleFunc("/echo", microsimserver.PushData)
+	router.HandleFunc("/", microsimserver.Home)
+
+	fmt.Println("Listening at port:5555")
+	openbrowser("http://127.0.0.1:5555")
+	http.ListenAndServe(":5555", router)
+}
+
+
+
+func SomeHandler(w http.ResponseWriter, r *http.Request) {
+    data := SomeStruct{}
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusCreated)
+    json.NewEncoder(w).Encode(data)
+}
+
+*/
